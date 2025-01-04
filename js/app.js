c8d259f591cba50e965b510e7801161a8bc25bac
User Name <user@email.com>
2025-01-04 15:45:51 -0500

    catch HTML element and store in variable , desclair snake and food position , create abject of array to store snake location. and adding event listner for keyboard arrow key and click for restart button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('hello')
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "white";
+const snakeColor = "lightgreen";
+const snakeBorder = "black";
+const foodColor = "red";
+const unitSize = 25;
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+let snake = [
+  { x: unitSize * 4, y: 0 },
+  { x: unitSize * 3, y: 0 },
+  { x: unitSize * 2, y: 0 },
+  { x: unitSize, y: 0 },
+  { x: 0, y: 0 },
+];
+//adding event listner for keyboard arrow key and click for restart button
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
+
+gameStart();
